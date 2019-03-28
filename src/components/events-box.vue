a4eb2d8474afebaf2e34a79d5a162e2c847f0b5b
bazelski <bazelski@gmail.com>
2019-03-28 15:23:36 +0100

    update

--- src/components/events-box.vue
+++ src/components/events-box.vue
@@ -3,12 +3,12 @@
     <div
       :key="index"
       class="event" 
-      :class="event.classes"
+      :class="event.classes +' '+event.icon"
       @click.stop="eventClicked(event)"
       v-for="(event, index) in eventList"
     >
       <div class="event-title">
-       <i class="event-icon fal" :class="event.icon"></i> {{ event.title }}
+       <i class="event-icon mr-1 far" :class="event.icon"></i> {{ event.title }}
       </div>
     </div>
     <span v-if="more" class="more-link" @click.stop="showAll()">
